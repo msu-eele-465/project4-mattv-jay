42ca25e973e1574f781eda84ffca4fc4db0d2b2d
Matthew Vandeberg <matvandeberg@gmail.com>
2025-03-24 23:14:22 -0600

    Move code from test files to main

--- i2c-led-bar/app/main.c
+++ i2c-led-bar/app/main.c
@@ -3,80 +3,68 @@
  * @brief Main file to run all code.
  */
 
-#include <msp430.h>
+#include <msp430fr2310.h>
 #include <stdint.h>
 #include <stdbool.h>
+
+#define I2C_ADDR 0x48
 
 unsigned int pattern_num = 0;
 uint8_t pattern = 0b00000000;
 
 bool unlocked = false;
+char key = '\0';
 
-int main(void)
+void init(void)
 {
-    char key;
-    int trans_period = 15625 - 1;
-    const float PER_FRACTIONS[] = {1.0, 1.0, 0.5, 0.5, 0.25, 1.5, 0.5, 1.0};
+    // Set ports 1.0, 1.1, 1.4-1.7, 2.0, 2.6, 2.7 as outputs
+    P1DIR |= BIT0 | BIT1 | BIT4 | BIT5 | BIT6 | BIT7;
+    P2DIR |= BIT0 | BIT6 | BIT7;
 
-    uint8_t pattern_store[] = {
-        {0b10101010},
-        {0b10101010},
-        {0b00000000},
-        {0b00011000},
-        {0b11111111},
-        {0b00000001},
-        {0b01111111},
-        {0b00000000}
-    };
+    // Set outputs to zero
+    P1OUT &= ~(BIT0 | BIT1 | BIT4 | BIT5 | BIT6 | BIT7);
+    P2OUT &= ~(BIT0 | BIT6 | BIT7);
 
-    const uint8_t PATTERNS[] = {
-        {0b10101010},
-        {0b10101010},
-        {0b00000000},
-        {0b00011000},
-        {0b11111111},
-        {0b00000001},
-        {0b01111111},
-        {0b00000000}
-    };
-
-    // Stop watchdog timer
-    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
-
-    //set ports 3.0-3.7 as outputs
-    //set P3OUT = 0b00000000
-
-    P3DIR |= BIT0 | BIT1 | BIT2 | BIT3 | BIT4 | BIT5 | BIT6 | BIT7;
-    P3OUT = 0b00000000;
-
-    // Configure switches
-    P4DIR &= ~BIT1;         // Clear P4.1 (S1) direction = in
-    P4REN |= BIT1;          // Enable pull up/down resistor
-    P4OUT |= BIT1;          // Make resistor a pull up
-    P4IES &= ~BIT1;         // Config IRQ Sensitivity L-to-H
-
-    P2DIR &= ~BIT3;         // Clear P2.3 (S2) direction = in
-    P2REN |= BIT3;          // Enable pull up/down resistor
-    P2OUT |= BIT3;          // Make resistor a pull up
-    P2IES &= ~BIT3;         // Config IRQ Sensitivity L-to-H
+    // I2C pins
+    P1SEL0 |= BIT2 | BIT3;
+    P1SEL1 &= ~(BIT2 | BIT3);
 
     // Disable the GPIO power-on default high-impedance mdoe to activate
     // previously configure port settings
     PM5CTL0 &= ~LOCKLPM5;
 
-    TB1CTL = TBSSEL__SMCLK | MC_1 | TBCLR | ID__8;  // SMCLK, up mode, clear TBR
-    TB1EX0 = TBIDEX__8;
+    TB0CTL = TBSSEL__SMCLK | MC_1 | TBCLR | ID__8; // SMCLK, up mode, clear TBR
+    TB0EX0 = TBIDEX__8;
+    TB0CCR0 = 15624; // Set up 1.0s period
+    TB0CCTL0 &= ~CCIFG; // Clear CCR0 Flag
+    TB0CCTL0 |= CCIE; // Enable TB0 CCR0 Overflow IRQ
 
-    // Set up 1.0s period
-    TB1CCR0 = trans_period;
+    UCB0CTLW0 = UCSWRST; // Software reset enabled
+    UCB0CTLW0 |= UCMODE_3 | UCSYNC; // I2C mode, sync mode
+    UCB0I2COA0 = I2C_ADDR | UCOAEN; // Own Address and enable
+    UCB0CTLW0 &= ~UCSWRST; // clear reset register
+    UCB0IE |= UCRXIE;
+    __enable_interrupt(); // Enable Maskable IRQs
+}
 
-    TB1CCTL0 &= ~CCIFG;     // Clear CCR0 Flag
-	TB1CCTL0 |= CCIE;       // Enable TB1 CCR0 Overflow IRQ
-	__enable_interrupt();   // Enable Maskable IRQs
+int main(void)
+{
+    int trans_period = 15625 - 1;
+    const float PER_FRACTIONS[] = { 1.0, 1.0, 0.5, 0.5, 0.25, 1.5, 0.5, 1.0 };
 
-    while(true)
+    uint8_t pattern_store[] = { { 0b10101010 }, { 0b10101010 }, { 0b00000000 }, { 0b00011000 },
+                                { 0b11111111 }, { 0b00000001 }, { 0b01111111 }, { 0b00000000 } };
+
+    const uint8_t PATTERNS[] = { { 0b10101010 }, { 0b10101010 }, { 0b00000000 }, { 0b00011000 },
+                                 { 0b11111111 }, { 0b00000001 }, { 0b01111111 }, { 0b00000000 } };
+
+    WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer
+
+    // Initialize ports and other subsystems
+    init();
+
+    while (true)
     {
-        
         if (unlocked)
         {
             if (key == 'A')
@@ -84,39 +72,41 @@ int main(void)
                 if (trans_period != 3906)
                 {
                     trans_period -= 3906;
+                    key = '\0';
                 }
             }
-            else if (key == 'B') 
+            else if (key == 'B')
             {
                 trans_period += 3906;
+                key = '\0';
             }
-            else if ((key - '0') >= 0 && (key - '0') < 8) 
+            else if ((key - '0') >= 0 && (key - '0') < 8)
             {
                 if (pattern_num == key - '0')
                 {
                     pattern = PATTERNS[(unsigned int)(key - '0')];
                 }
-                else 
+                else
                 {
                     pattern_store[pattern_num] = pattern;
                     pattern = pattern_store[(unsigned int)(key - '0')];
                     pattern_num = (unsigned int)(key - '0');
                 }
-                P3OUT = pattern;
+                // Set LED bar outputs
+                P1OUT = (P1OUT & 0b11111100) | (pattern & 0b00000011);
+                P1OUT = (P1OUT & 0b00001111) | ((pattern & 0b00111100) << 2);
+                P2OUT = (P2OUT & 0b00111111) | (pattern & 0b11000000);
+                key = '\0';
             }
-        }
-
-        if (pattern_num < 8)
-        {
-            TB1CCR0 = (unsigned int)(trans_period * PER_FRACTIONS[pattern_num]);
+            TB0CCR0 = (unsigned int)(trans_period * PER_FRACTIONS[pattern_num]);
         }
     }
 }
 
-#pragma vector = TIMER1_B0_VECTOR
-__interrupt void ISR_TB1_CCR0(void) 
+#pragma vector = TIMER0_B0_VECTOR
+__interrupt void ISR_TB0_CCR0(void)
 {
-switch (pattern_num)
+    switch (pattern_num)
     {
         case 1:
             pattern = ~pattern;
@@ -145,7 +135,7 @@ switch (pattern_num)
             {
                 pattern = 0b01111111;
             }
-            else 
+            else
             {
                 pattern = pattern >> 1;
                 pattern ^= 0b10000000;
@@ -160,7 +150,21 @@ switch (pattern_num)
             pattern ^= 0b00000001;
             break;
     }
-    P3OUT = pattern;
 
-    TB0CCTL0 &= ~CCIFG;     // Clear CCR0 Flag
+    // Set LED bar outputs
+    P1OUT = (P1OUT & 0b11111100) | (pattern & 0b00000011);
+    P1OUT = (P1OUT & 0b00001111) | ((pattern & 0b00111100) << 2);
+    P2OUT = (P2OUT & 0b00111111) | (pattern & 0b11000000);
+
+    TB0CCTL0 &= ~CCIFG; // Clear CCR0 Flag
+}
+
+#pragma vector = EUSCI_B0_VECTOR
+__interrupt void EUSCI_B0_I2C_ISR(void)
+{
+    key = UCB0RXBUF;
+    if (key == 'U')
+    {
+        unlocked = true;
+    }
 }
